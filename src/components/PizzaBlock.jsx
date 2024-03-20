a2e02ea47507e102057bca6f0b9094d3387b63ea
BrainWosher <svprokopenko92@gmail.com>
2024-03-20 20:39:08 +0700

    картинки и импорт лого

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,7 +1,7 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 const PizzaBlock = ({title, price}) => {
-  const [pizzaCount, setPizzaCount] = useState(0);
+  const [pizzaCount, setPizzaCount] = React.useState(0);
   return (
     <div className="pizza-block">
       <img
