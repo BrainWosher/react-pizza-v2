e4ea3ee4a91eedfda3002ecd533f041b3fe8ddce
BrainWosher <svprokopenko92@gmail.com>
2024-04-14 16:09:21 +0700

    компонент скелетона и его отображение

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
+import Skeleton from './components/PizzaBlock/Skeleton';
 
 function App() {
   const [pizzas, setPizzas] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
 
   React.useEffect(() => {
     fetch(`https://659657ee6bb4ec36ca026645.mockapi.io/pizzas/`)
       .then((res) => res.json())
       .then((json) => {
         setPizzas(json);
+        setIsLoading(false);
       })
       .catch((err) => {
         console.warn(err);
@@ -32,9 +35,9 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((obj) => (
-              <PizzaBlock key={obj.id} {...obj} />
-            ))}
+            {isLoading
+              ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+              : pizzas.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
           </div>
         </div>
       </div>
