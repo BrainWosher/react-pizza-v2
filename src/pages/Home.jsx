b1689f5b8d9fbbe45feca21c67395f8fd60760fe
BrainWosher <svprokopenko92@gmail.com>
2024-04-14 21:03:15 +0700

    react router и роутинг

---
+++ src/pages/Home.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import Categories from '../components/Categories';
+import Sort from '../components/Sort';
+import PizzaBlock from '../components/PizzaBlock';
+import Skeleton from '../components/PizzaBlock/Skeleton';
+
+const Home = () => {
+  const [pizzas, setPizzas] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch(`https://659657ee6bb4ec36ca026645.mockapi.io/pizzas/`)
+      .then((res) => res.json())
+      .then((json) => {
+        setPizzas(json);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert('Ошибка при получении данных');
+      })
+      .finally(console.log('Данные загрузились'));
+  }, []);
+
+  return (
+    <div>
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : pizzas.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
