e9a9a0a3e412a84c690d779c8355dab0a397acfd
BrainWosher <svprokopenko92@gmail.com>
2024-04-18 17:54:05 +0700

    заглушка для страницы корзина

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import './scss/app.scss';
 import Header from './components/Header';
 import Home from './pages/Home';
+import Cart from './pages/Cart';
 import NotFound from './pages/NotFound';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
