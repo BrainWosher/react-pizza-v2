70a6c66c7c316affe7b60475b467b4acf5bb180e
BrainWosher <svprokopenko92@gmail.com>
2024-09-04 21:15:33 +0700

    контекст поиска

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import logoSvg from '../assets/img/pizza-logo.svg';
 import Search from './Search';
 
-const Header = ({ searchValue, setSearchValue }) => (
+const Header = () => (
   <div className="header">
     <div className="container">
       <Link to="/">
@@ -15,7 +15,7 @@ const Header = ({ searchValue, setSearchValue }) => (
           </div>
         </div>
       </Link>
-      <Search searchValue={searchValue} setSearchValue={setSearchValue} />
+      <Search />
       <div className="header__cart">
         <Link to="/cart" className="button button--cart">
           <span>520 ₽</span>
