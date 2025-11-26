c8374e9092419ad438e1924f3fd6b3dea97d261b
BrainWosher <svprokopenko92@gmail.com>
2025-11-26 21:45:18 +0700

    удалены  ненужные импорты

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import qs from 'qs';
-import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 import Categories from '../components/Categories';
-import Sort, { sortList } from '../components/Sort';
+import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
 import Pagination from '../components/Pagination';
@@ -15,10 +13,8 @@ import { filterSelector } from '../redux/filter/selectors';
 import { fetchPizzas } from '../redux/pizza/asyncActions';
 
 const Home: React.FC = () => {
-  const navigate = useNavigate();
   const dispatch = useAppDispatch();
   const isSearch = React.useRef(false);
-  const isMounted = React.useRef(false);
 
   const { items, status } = useSelector(pizzaSelector);
   const { categoryId, sort, currentPage, searchValue } = useSelector(filterSelector);
@@ -50,38 +46,6 @@ const Home: React.FC = () => {
     );
   };
 
-  // //Не пихать парметры в адресную строку на первом рендере
-  // React.useEffect(() => {
-  //   if (isMounted.current) {
-  //     const queryString = qs.stringify({
-  //       sortProperty: sort.sortProperty,
-  //       categoryId,
-  //       currentPage,
-  //     });
-
-  //     navigate(`?${queryString}`);
-  //   }
-  //   isMounted.current = true;
-  // }, [categoryId, sort.sortProperty, currentPage, navigate]);
-
-  // //Если был первый рендер, проверить url-параметры и сохранить в редакс
-  // React.useEffect(() => {
-  //   if (window.location.search) {
-  //     const params = qs.parse(window.location.search.substring(1));
-
-  //     const sort = sortList.find((obj) => obj.sortProperty === params.sortProperty);
-
-  //     dispatch(
-  //       setFilters({
-  //         ...params,
-  //         sort: sort || sortList[0],
-  //       }),
-  //     );
-  //     isSearch.current = true;
-  //   }
-  // }, []);
-
-  //Если был первый рендер, дернуть завпрос
   React.useEffect(() => {
     window.scrollTo(0, 0);
 
